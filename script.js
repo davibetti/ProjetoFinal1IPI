1acf59def79395d175d9dc298d4f13ead41a61da
davibetti <davibettioliveira@gmail.com>
2023-09-13 17:04:37 -0300

    new file:   index.html 	new file:   script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+var lista_de_produtos = []
+
+
+
+while(true){
+
+var opcao = prompt(`
+
+ bem vindo ao cadastro de produtos
+
+ 1. cadastrar novo produto
+ 2.editar produto
+ 3.remover produto
+ 4.visualizar todos os produtos
+
+`)
+
+if(opcao == 1){
+  // codigo para adcionar produto
+}else if(opcao == 2){
+  // codigo para editar produto    
+}else if(opcao == 3){
+  //codigo para remover produto
+}else if(opcao == 4){
+  //codigo para visualizar
+}else{
+
+alert("nenhuma das opções foi selecionada !")
+
+}
+}
