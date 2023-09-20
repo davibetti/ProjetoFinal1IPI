2a388aec2aaacf942616024447e3262f4c078e7a
davibetti <144274518+davibetti@users.noreply.github.com>
2023-09-20 17:50:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,87 @@
-var lista_de_produtos = []
+// array ou [] serve para armazenar diferentes valores dentro da variavel
+var lista = []
 
 
-
+// while serve para criar um loop , e o true é para em quanto for verdadeiro rodar o codigo
 while(true){
 
-var opcao = prompt(`
+// chaves ou {} serve para mostrar o que é de cada codigo, ``serve para deixar mais organizado o texto
 
- bem vindo ao cadastro de produtos
+var nome=prompt(`1 - Adicionar
 
- 1. cadastrar novo produto
- 2.editar produto
- 3.remover produto
- 4.visualizar todos os produtos
+2 - Editar 
 
-`)
+3 - Excluir
 
-if(opcao == 1){
-  // codigo para adcionar produto
-}else if(opcao == 2){
-  // codigo para editar produto    
-}else if(opcao == 3){
-  //codigo para remover produto
-}else if(opcao == 4){
-  //codigo para visualizar
+4 - Vizualizar
+
+5 - Sair`)
+
+//if serve para mostra decisão
+if(nome == "1"){
+
+adicionar()
+
+//else serve para caso não seja if seja else , if = se, else = se não 
+}else if(nome == "2"){
+
+editar()
+}else if(nome == "3"){
+
+// () serve para indicar que é uma função
+excluir() 
+}else if(nome =="4"){
+
+visualizar()
+}else if(nome == "5"){
+
+//break serve para quebrar o while
+break;
+
 }else{
 
-alert("nenhuma das opções foi selecionada !")
+}
 
 }
+
+
+function adicionar(){
+    var produto = {
+        
+        nome:prompt("Digite o nome do produto:"),
+        marca: prompt("Digite a marca do produto:"),
+        preco:prompt("Digite o preco do produto:")
+    }
+    // o sifrão ou $ serve colocar uma var dentro do texto
+    var texto = `Nome: ${produto.nome}
+    \n Marca: ${produto.marca}
+    \n Preço: ${produto.preco}`;
+    lista.push(texto);
+    alert("Produto Adicionado!");
+
+}
+function editar(){
+var edit = prompt("edite o produto desejado")
+var produto = {
+        
+    nome:prompt("Digite o nome do produto:"),
+    marca: prompt("Digite a marca do produto:"),
+    preco:prompt("Digite o preco do produto:")
+}
+// o sifrão ou $ serve colocar uma var dentro do texto
+var texto = `Nome: ${produto.nome}
+\n Marca: ${produto.marca}
+\n Preço: ${produto.preco}`;
+lista[edit]=texto
+} 
+
+function visualizar(){
+    //join serve para listar
+    alert(lista.join("\n"))
+}
+
+function excluir(){
+    var edit = prompt("edite o produto desejado")
+    //splice serve para remover ,edit é o indice que ele deseja remover e o 1 é a quantidade que ele dejesa remover
+    lista.splice(edit,1);
 }
